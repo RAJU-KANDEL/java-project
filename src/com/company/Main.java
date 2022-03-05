893196a052ff6c1c4d2cc76a49e66fa533849e41
raju kandel <kandel.raju999@gmail.com>
2022-03-05 18:46:18 +0900

    my pc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+            Scanner scanner= new Scanner(System.in);
+            System.out.print("Enter year: ");
+            int year =scanner.nextInt();
+            boolean c = isLeapYear(year);
+            if(c){
+                System.out.println("leap year");
+            }
+            else {
+                System.out.println("Not leap year");
+            }
+            System.out.println(c);
+        }
+
+
+        public static boolean isLeapYear(int year) {
+            if(year <1 || year>9999){
+                return false;
+            }
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return true;
+                }
+            }else {
+                return false;
+            }
+        }
     }
-}
+
